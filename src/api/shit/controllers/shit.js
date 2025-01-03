22e2b026ce1c504b36e8fc2f162e0fb978ff5784
junhyuk <junhyuk@flex.team>
2025-01-03 20:38:39 +0900

    feat(blog): test

---
+++ src/api/shit/controllers/shit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shit controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::shit.shit');
---
+++ src/api/shit/services/shit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shit service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::shit.shit');
